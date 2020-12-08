b58a02b31b2224c67e65db5046d792b3e24bb935
Janice Guo <janiceguo@berkeley.edu>
2020-12-08 01:42:13 -0500

    main

--- Main.java
+++ Main.java
@@ -17,7 +17,7 @@ public class Main {
             throw new IllegalArgumentException("must be a normal file");
         }
         try {
-            ArrayList<String> lines = new ArrayList<>(Files.readAllLines(Paths.get("inputs4/" + file.getName())));
+            ArrayList<String> lines = new ArrayList<>(Files.readAllLines(Paths.get("inputs2/" + file.getName())));
             return lines;
         }
         catch (IOException e) {
@@ -128,7 +128,7 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND> .... */
     public static void main(String... args) {
-        File inputs = new File("inputs4");
+        File inputs = new File("inputs2");
         if (!inputs.isDirectory()) {
             throw new IllegalArgumentException("inputs must be directory");
         }
